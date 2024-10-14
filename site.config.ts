2962f1b8e01a6cf9f9a977f74a795a7ea885d362
Michael <mamatoya@asu.edu>
2024-10-14 11:51:37 -0700

    Update site.config.ts
    Database ID

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '105f899d682680f381fdefbd9e8a6946?v=66549cd79bb94b9ebf555c547193539e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
